596987969642ffda876acb8d804202b64ae1b245
AbdealiJK <abdealikothari@gmail.com>
2016-02-28 00:02:08 +0530

    logs: Add functionality
    Fixes https://github.com/coala-analyzer/coala-html/issues/15

--- app/controllers/logs.js
+++ app/controllers/logs.js
@@ -1,5 +1,14 @@
 'use strict';
 
 angular.module('coalaHtmlApp')
-  .controller('LogsCtrl', function () {
+  .controller('LogsCtrl', function ($scope, $http) {
+    var parseCoalaProject = function(projectDir, coalaJSON) {
+      coalaJSON = coalaJSON || projectDir + '/coala.json';
+      $scope.data = [];
+      $http.get(coalaJSON).success(function(response){
+        $scope.data = response.logs;
+      });
+    };
+    $scope.LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR"];
+    parseCoalaProject("tests/test_projects/simple");
   });
