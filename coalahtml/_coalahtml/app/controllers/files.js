0c0738af0a95540115721a4b36272afc1014e16f
Tushar Gautam <tushar.rishav@gmail.com>
2016-05-22 20:06:39 +0530

    files.js: Create file hirerachy
    This commit:
    1.Replaces `tests/test_projects/*` with
    `data` directory.
    2.Create file hierarchy using `data/` files
    
    Refs https://github.com/coala-analyzer/coala-html/issues/31

--- coalahtml/_coalahtml/app/controllers/files.js
+++ coalahtml/_coalahtml/app/controllers/files.js
@@ -7,32 +7,45 @@ angular.module('coalaHtmlApp')
        * JSON object with keys as directory relative to the project
        * path and values are files inside directory relative to the
        * project.
-       * For files in the project root, the key should be "".
+       * For files in the project root, the key should be root.
        */
-      filesJSON = filesJSON || projectDir + '/files.json';
+      filesJSON = filesJSON || projectDir + $scope.Constants.files;
       $scope.data = [];
       $scope.fileBack = fileName.split("/").slice(0, -1).join('/');
-
-      $http.get(filesJSON).success(function(fileData){
-        if (fileData.hasOwnProperty(fileName)) {
+      $http.get(filesJSON).success(function(fJSON) {
+        if (fJSON.hasOwnProperty(fileName)) {
           $scope.fileType = "dir";
           $scope.fileContents = {};
-          for(var i_file = 0; i_file < fileData[fileName].length; i_file += 1) {
-            var dirFile = fileData[fileName][i_file];
-            var dirFileType = fileData.hasOwnProperty(dirFile) ? "dir": "file";
+          for(var i_file = 0;i_file < fJSON[fileName].length;i_file += 1) {
+            var dirFile = fJSON[fileName][i_file];
+            var dirFileType = fJSON.hasOwnProperty(dirFile) ? "dir": "file";
             $scope.fileContents[dirFile] = dirFileType;
           }
         } else {
-          $scope.fileType = "file";
-          $http.get(projectDir + "/" + fileName)
-            .success(function(content) {
-              $scope.fileContents = content;
+            $scope.fileType = "file";
+            $http.get(projectDir + $scope.Constants.file_data)
+            .success(function(fileData) {
+              var result = "";
+              fileData[fileName].forEach(function(line){
+                result += line;
+              });
+              $scope.fileContents = result;
             });
         }
-        $scope.data = fileData;
+        $scope.data = fJSON;
+      }).error(function(e){
+        console.log(e);
       });
     };
-
-    $scope.fileName = $routeParams.fileName || "";
-    parseCoalaProject($scope.fileName, "tests/test_projects/simple");
+    $http.get("data/Constants.json").success(function(constants) {
+      $scope.Constants = constants;
+      $http.get($scope.Constants.roothome).success(function(roothome) {
+        $scope.fileName = $routeParams.fileName || roothome;
+        $scope.roothome = roothome;
+        parseCoalaProject($scope.fileName, $scope.Constants.data);
+      });
+    });
+    $scope.basename = function(path_name){
+      return path_name.split('/').slice(-1)[0];
+    };
   });
