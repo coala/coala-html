0d2c413430a487b3e5fc2e39f8400ffb2cd6f55a
Tushar Gautam <tushar.rishav@gmail.com>
2016-05-22 20:14:51 +0530

    main.js: Display all sourceRange results
    Refs https://github.com/coala-analyzer/coala-html/issues/31

--- coalahtml/_coalahtml/app/controllers/main.js
+++ coalahtml/_coalahtml/app/controllers/main.js
@@ -3,50 +3,39 @@
 angular.module('coalaHtmlApp')
   .controller('MainCtrl', function ($scope, $http) {
     var parseCoalaProject = function(projectDir, coalaJSON) {
-      coalaJSON = coalaJSON || projectDir + '/coala.json';
-      $scope.data = [];
-      $http.get(coalaJSON).success(function(response){
-        var knownFiles = {};
-        $scope.content = {};
-
-        var parseResult = function(el){
-          el.affected_code.forEach(function(sourceRange){
-              if (!(sourceRange.file in knownFiles)){
-                  knownFiles[sourceRange.file] = [];
-
-                  $http.get(projectDir + "/" + sourceRange.file)
-                    .success(function(content) {
-                      $scope.content[sourceRange.file] = content.split("\n");
+      $http.get(projectDir + $scope.Constants.file_data)
+        .success(function(file_data) {
+          $scope.file_dict = angular.copy(file_data);
+          coalaJSON = coalaJSON || projectDir + $scope.Constants.coala;
+          $scope.data = [];
+          $http.get(coalaJSON).success(function(coala_json) {
+            var knownFiles = {};
+            var parseResult = function(el) {
+                el.affected_code.forEach(function(sourceRange){
+                    if (!(sourceRange.file in knownFiles)){
+                        knownFiles[sourceRange.file] = [];
+                    }
+                    knownFiles[sourceRange.file].push({
+                      "start":    sourceRange.start.line,
+                      "end":      sourceRange.end.line,
+                      "diffs":    el.diffs,
+                      "message":  el.message,
+                      "origin":   el.origin,
+                      "severity": el.severity
                     });
+                });
+            };
+            for (var section in coala_json.results) {
+              if (coala_json.results[section].length > 0) {
+                coala_json.results[section].forEach(parseResult);
               }
-              knownFiles[sourceRange.file].push({
-                "start":    sourceRange.start.line,
-                "end":      sourceRange.end.line,
-                "diffs":    el.diffs,
-                "message":  el.message,
-                "origin":   el.origin,
-                "severity": el.severity
-              });
+            }
+            $scope.data.push(knownFiles);
           });
-        };
-
-        for (var errors in response.results) {
-          if (response.results[errors][0]){ // If error exists
-              response.results[errors].forEach(parseResult);
-          }
-        }
-
-        $scope.data.push(knownFiles);
-      });
-    };
-    parseCoalaProject("tests/test_projects/simple");
-
-    $scope.range = function(min, max, step) {
-      step = step || 1;
-      var input = [];
-      for (var i = min; i <= max; i += step) {
-        input.push(i);
-      }
-      return input;
-    };
+        });
+      };
+    $http.get("data/Constants.json").success(function(constants) {
+      $scope.Constants = constants;
+      parseCoalaProject($scope.Constants.data);
+    });
   });
