e04945d1b74c33a7fea9fa8ecae549efea1bb7fb
Tushar Gautam <tushar.rishav@gmail.com>
2016-05-22 20:02:26 +0530

    .coafile: Fix the files as per the added Bear
    Refs https://github.com/coala-analyzer/coala-html/issues/31

--- coalahtml/_coalahtml/tests/test_projects/simple/test.py
+++ coalahtml/_coalahtml/tests/test_projects/simple/test.py
@@ -1,13 +1,12 @@
 import sys
-entry_point=sys.argv[1]
+entry_point = sys.argv[1]
 print sys.argv
 for entry in ['coala-json']:
     if entry_point.endswith(entry):
-        parser_type=entry
+        parser_type = entry
         break
 else:
-    parser_type='coala'
+    parser_type = 'coala'
 # print
 # todo
 # fixme
-print parser_type
