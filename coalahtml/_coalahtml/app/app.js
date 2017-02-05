eff9c235ed4478260244b81092e3817aefd59daf
Hemang Kumar <hemangsk@gmail.com>
2017-02-05 12:20:07 +0100

    app.js: Fix URLs hashing

--- coalahtml/_coalahtml/app/app.js
+++ coalahtml/_coalahtml/app/app.js
@@ -26,7 +26,9 @@ angular
       });
     });
   }])
-  .config(['$routeProvider', function ($routeProvider) {
+  .config(['$routeProvider', '$locationProvider',
+           function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('');
     $routeProvider
       .when('/', {
         templateUrl: 'app/views/main.html',
